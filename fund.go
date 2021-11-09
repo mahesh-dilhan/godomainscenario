d15ead4faa190d3412da6d242abdddf1ca0568fd
mahesh <mail.mahesh.lk@gmail.com>
2021-11-09 09:07:07 +0800

    fund domain

--- fund.go
+++ fund.go
@@ -4,3 +4,12 @@ type Fund struct {
 	// balance is unexported (private), because it's lowercase
 	balance int
 }
+
+// A regular function returning a pointer to a fund
+func NewFund(initialBalance int) *Fund {
+	// We can return a pointer to a new struct without worrying about
+	// whether it's on the stack or heap: Go figures that out for us.
+	return &Fund{
+		balance: initialBalance,
+	}
+}
